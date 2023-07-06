6ccd1ffb497d3ea15812e4713d084f8f1f0ce863
Matheus Gomes Rocha <81640187+MattiyyahG@users.noreply.github.com>
2023-07-06 20:09:29 -0300

    Create Banco_de_Dados.py

---
+++ Banco_de_Dados.py
@@ -0,0 +1,38 @@
+import sqlite3
+import paho.mqtt.client as mqtt
+
+# Função para lidar com a mensagem recebida via MQTT
+def on_message(client, userdata, msg):
+    # Decodifica a mensagem recebida
+    sensor_data = msg.payload.decode()
+    
+    # Divide a mensagem em valores separados
+    values = sensor_data.split(',')
+    temperatura = float(values[0])
+    luminosidade = float(values[1])
+    co2 = float(values[2])
+    
+    # Insere os valores no banco de dados
+    conn = sqlite3.connect('sensor_data.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO dados_sensores (temperatura, luminosidade, co2) VALUES (?, ?, ?)',
+                   (temperatura, luminosidade, co2))
+    conn.commit()
+    conn.close()
+
+# Configurações do broker MQTT
+broker_address = 'endereco_do_broker'
+topic = 'sensores'
+
+# Conecta ao broker MQTT
+client = mqtt.Client()
+client.connect(broker_address)
+
+# Associa a função de callback ao evento on_message
+client.on_message = on_message
+
+# Inscreve-se no tópico para receber as mensagens dos sensores
+client.subscribe(topic)
+
+# Inicia o loop para aguardar as mensagens
+client.loop_start()
